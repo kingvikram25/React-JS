35214c22802a5c552c2f5413058bb075b35f356b
Vikram Singh <sikarwarvikram88@gmail.com>
2023-07-20 12:45:36 +0530

    code updated

--- crypto-currency/src/App.js
+++ crypto-currency/src/App.js
@@ -1,20 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
 import Coin from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
   useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false",
-      )
-      .then((res) => {
-        setCoins(res.data);
-        console.log(res.data);
-      })
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+      { method: "GET" },
+      { mode: "no-cors" },
+    )
+      .then((res) => res.json())
+      .then((data) => setCoins(data))
       .catch((error) => console.log(error));
   }, []);
   const handleChange = (e) => {
@@ -39,16 +37,16 @@ function App() {
       </div>
       <div className="title-heading">
         <p className="title-name">Name</p>
-        <p className="title-price">Current Price</p>
+        <p className="title-price">Coin Price</p>
         <p> Change's (24hr)</p>
         <p>Market Cap</p>
         <p>Currency Rank</p>
       </div>
+
       {filteredCoins.map((coin) => {
         return (
-          <>
+          <Fragment key={coin.id}>
             <Coin
-              key={coin.id}
               name={coin.name}
               image={coin.image}
               symbol={coin.symbol}
@@ -57,7 +55,7 @@ function App() {
               pricechange={coin.price_change_percentage_24h}
               marketRank={coin.market_cap_rank}
             />
-          </>
+          </Fragment>
         );
       })}
     </div>
